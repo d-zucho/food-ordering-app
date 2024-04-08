6bdb8175ce9828517f480e622f73e7cba3725b83
d-zucho <danny.nicodev@gmail.com>
2024-04-07 22:41:00 -0700

    add header and nav

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header className='flex items-center justify-between'>
+      <Link className='text-primary font-semibold text-2xl' href='/'>
+        St Pizza
+      </Link>
+
+      <nav className='flex items-center gap-8 text-gray-500 font-semibold'>
+        <Link href='/'>Home</Link>
+        <Link href='/'>Menu</Link>
+        <Link href='/'>About</Link>
+        <Link href='/'>Contact</Link>
+        <Link href='/' className='bg-primary text-white px-8 py-2 rounded-full'>
+          Login
+        </Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
